d4b75bcf7072639df16b22d24b045b6832d1e0c9
Md Adnan Umar <adnanmd0786@gmail.com>
2025-10-02 23:30:39 +0530

    Add .gitignore and update Application.java

--- src/Application.java
+++ src/Application.java
@@ -4,17 +4,16 @@ import java.awt.EventQueue;
 import GUI.GUIForm;
 
 public class Application {
-	public static void main(String[] args) 
-		{
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						
-						GUIForm.login.frame.setVisible(true);
-					} catch (Exception e) {
-						
-					}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+
+					GUIForm.login.frame.setVisible(true);
+				} catch (Exception e) {
+
 				}
-			});
-		}
+			}
+		});
+	}
 }
